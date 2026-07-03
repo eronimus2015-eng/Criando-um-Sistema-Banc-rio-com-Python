13a69dd111dca7a0383df0d96d53c6c150cdc4f7
Luiz Cesar de Lima <eronimus2015@gmail.com>
2026-07-03 13:41:21 -0300

    Improve error messages for withdrawal operations

--- desafio banco/desafio banco.py
+++ desafio banco/desafio banco.py
@@ -31,11 +31,11 @@ while True:
         excedeu_saques = numero_saques >= LIMITE_SAQUES
 
         if excedeu_saldo:
-            print("Operação falhou! Saldo insuficiente.")
+            print("Operação não foi realizada seu saldo é insuficiente.")
         elif excedeu_limite:
-            print("Operação falhou! O limite máximo por saque é R$500.")
+            print("Operação não pode ser realizada pois o limite máximo por saque é R$500.")
         elif excedeu_saques:
-            print("Operação falhou! Limite diário de 3 saques atingido.")
+            print("Operação incocluida pois o Limite diário de 3 saques ja foi atingido.")
         elif valor > 0:
             saldo -= valor
             extrato += f"Saque: R${valor:.2f}\n"
